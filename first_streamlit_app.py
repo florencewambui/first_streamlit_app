82187dc4773c0aa6d25d883a54daadf155b5211f
florencewambui <32742538+florencewambui@users.noreply.github.com>
2023-02-15 08:58:36 +0300

    Update first_streamlit_app.py
    Add a list of fruits for customers to choose from and make it interactive.

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas as pd
+
 streamlit.title('Snowflake Learning Diner')
 
 streamlit.header('Breakfast Menu')
@@ -6,3 +8,10 @@ streamlit.text('Blueberry Oatmeal Pancakes')
 streamlit.text('Smoothies')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#import csv file from aws bucket containing fruits data
+fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = fruit_list.set_index('Fruit')
+fruits_selected = streamlit.multiselect("Choose the fruits you would like in your smoothie: ", list(fruit_list.index), ['Apple', 'Banana', 'Grapes'])
+fruits_to_show = fruit_list.loc[fruits_selected]
+pd.dataframe(fruits_to_show)
